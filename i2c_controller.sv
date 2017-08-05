3222fd057a096dad553c6aea5ac12eea949eaee6
jomonkjoy <jomonkurianjoy@gmail.com>
2017-08-05 16:46:07 +0530

    Update i2c_controller.sv

--- i2c_controller.sv
+++ i2c_controller.sv
@@ -44,6 +44,7 @@ module i2c_controller #(
   localparam DATA_LENGTH = 9*4;
   localparam COUNT_WIDTH = $clog2(9);
   logic [COUNT_WIDTH-1:0] count = {COUNT_WIDTH{1'b0}};
+  logic [BYTE_WIDTH-1:0] count_byte = {BYTE_WIDTH{1'b0}};
   
   logic [7:0]            dev_id_r;
   logic [DATA_WIDTH-1:0] address_r;
@@ -67,19 +68,30 @@ module i2c_controller #(
     end
   end
   
+  logic read_write_n;
+  assign read_write_n = dev_id_r[0]; // R/W_bar
+  
+  logic restart_done = 0;
+  
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      restart_done <= 0;
+    end else if (state == DONE) begin
+      restart_done <= 0;
+    end else if (!restart_done && state == RESTART) begin
+      restart_done <= 1;
+    end
+  end
+  
   logic request_pending = 1'b0;
-  logic write_operation = 1'b0;
   
   always_ff @(posedge clk) begin
     if (reset) begin
       request_pending <= 0;
-      write_operation <= 0;
     end else if (state == DONE) begin
       request_pending <= 0;
-      write_operation <= 0;
     end else if (!request_pending && state == IDLE && access_request) begin
       request_pending <= 1;
-      write_operation <= !dev_id[0]; // R/W_B
     end
   end
   
@@ -115,16 +127,59 @@ module i2c_controller #(
             count <= count + 1;
           end
         end
-        DEV_ADDR : begin
-          if (count >= DATA_LENGTH-1) begin
+        RESTART : begin
+          if (count >= COMD_LENGTH-1) begin
             state <= DEV_ADDR;
             count <= {COUNT_WIDTH{1'b0}};
           end else begin
             count <= count + 1;
           end
         end
+        DEV_ADDR : begin
+          if (count >= DATA_LENGTH-1 && restart_done) begin
+            state <= READ_DATA;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else if (count >= DATA_LENGTH-1) begin
+            state <= REG_ADDR;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else begin
+            count <= count + 1;
+          end
+        end
+        REG_ADDR : begin
+          if (count >= DATA_LENGTH-1 && read_write_n) begin
+            state <= RESTART;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else if (count >= DATA_LENGTH-1) begin
+            state <= WRITE_DATA;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else begin
+            count <= count + 1;
+          end
+        end
+        READ_DATA : begin
+          if (count >= DATA_LENGTH-1) begin
+            state <= STOP;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else begin
+            count <= count + 1;
+          end
+        end
+        WRITE_DATA : begin
+          if (count >= DATA_LENGTH-1) begin
+            state <= STOP;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else begin
+            count <= count + 1;
+          end
+        end
         STOP : begin
-          state <= DONE;
+          if (count >= COMD_LENGTH-1) begin
+            state <= DONE;
+            count <= {COUNT_WIDTH{1'b0}};
+          end else begin
+            count <= count + 1;
+          end
         end
         DONE : begin
           state <= IDLE;
@@ -133,6 +188,71 @@ module i2c_controller #(
         default : begin
           state <= IDLE;
           count <= {COUNT_WIDTH{1'b0}};
+        end
+      endcase
+    end
+  end
+  
+  // Driving SCL output and output_en
+  always_ff @(posedge clk) begin
+    case (state)
+      IDLE, DONE : begin
+        scl_o <= 1'b1; scl_oe <= 1'b0;
+      end
+      START, RESTART : begin
+        case (count[1:0])
+          2'b00 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b01 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b10 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b11 : begin scl_o <= 1'b0; scl_oe <= 1'b1; end
+        endcase
+      end
+      STOP : begin
+        case (count[1:0])
+          2'b00 : begin scl_o <= 1'b0; scl_oe <= 1'b1; end
+          2'b01 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b10 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b11 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+        endcase
+      end
+      default : begin
+        case (count[1:0])
+          2'b00 : begin scl_o <= 1'b0; scl_oe <= 1'b1; end
+          2'b01 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b10 : begin scl_o <= 1'b1; scl_oe <= 1'b1; end
+          2'b11 : begin scl_o <= 1'b0; scl_oe <= 1'b1; end
+        endcase
+      end
+    endcase
+  end
+  
+  // Driving SDA output and output_en
+  always_ff @(posedge clk) begin
+    if (count == DATA_LENGTH-1) begin
+      sda_o <= 1'b1; sda_oe <= 1'b1;
+    end else begin
+      case (state)
+        START, RESTART : begin
+          case (count[1:0])
+            2'b00 : begin sda_o <= 1'b1; sda_oe <= 1'b1; end
+            2'b01 : begin sda_o <= 1'b1; sda_oe <= 1'b1; end
+            2'b10 : begin sda_o <= 1'b0; sda_oe <= 1'b1; end
+            2'b11 : begin sda_o <= 1'b0; sda_oe <= 1'b1; end
+          endcase
+        end
+        STOP : begin
+          case (count[1:0])
+            2'b00 : begin sda_o <= 1'b0; sda_oe <= 1'b1; end
+            2'b01 : begin sda_o <= 1'b0; sda_oe <= 1'b1; end
+            2'b10 : begin sda_o <= 1'b1; sda_oe <= 1'b1; end
+            2'b11 : begin sda_o <= 1'b1; sda_oe <= 1'b1; end
+          endcase
+        end
+        DEV_ADDR, REG_ADDR, WRITE_DATA : begin
+          sda_o <= buffer[DATA_WIDTH-1]; sda_oe <= 1'b1;
+        end
+        default : begin
+          sda_o <= 1'b1; sda_oe <= 1'b0;
         end
       endcase
     end
